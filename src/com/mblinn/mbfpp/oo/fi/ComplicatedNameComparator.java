04fc1219cc848cea155c08baf36f3c669a38007e
OverDriveMC <mengcheng1994@gmail.com>
2015-10-26 19:27:35 +0800

    functionSchemaJava sort Person by name

---
+++ src/com/mblinn/mbfpp/oo/fi/ComplicatedNameComparator.java
@@ -0,0 +1,12 @@
+package com.mblinn.mbfpp.oo.fi;
+
+import java.util.Comparator;
+
+public class ComplicatedNameComparator implements Comparator<Person> {
+	@Override
+	public int compare(Person arg0, Person arg1) {
+		/*
+		 * complicatedSortLogic*/
+		return 0;
+	}
+}
